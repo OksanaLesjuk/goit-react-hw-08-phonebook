def3beccf713e101ad7b5ab0fcbaca09c48f716f
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-10-27 22:34:00 +0300

    Update ContactsPage.js

--- src/pages/ContactsPage/ContactsPage.js
+++ src/pages/ContactsPage/ContactsPage.js
@@ -1,5 +1,5 @@
 import { Button, Typography } from '@mui/material';
-import { ContactForm } from 'components/ContactForm/ContactForm';
+
 import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
 import React, { useEffect, useState } from 'react'
